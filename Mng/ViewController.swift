7efca32ce6d94ce2252292ca8768cc0951f2dc98
vale <changweitu@gmail.com>
2015-04-19 06:18:55 +0800

    Initial Commit

---
+++ Mng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mng
+//
+//  Created by vale on 4/19/15.
+//  Copyright (c) 2015 changweitu@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
